beda1feb3bdd8e5de1655b0aaed1a9d62a3bb760
KalashShah90 <138599698+KalashShah90@users.noreply.github.com>
2023-10-08 00:14:18 +0530

    Update crop_advisor.py

--- crop_advisor.py
+++ crop_advisor.py
@@ -2,19 +2,9 @@ import streamlit as st
 import pickle
 import numpy as np
 
-import os
-
-file_path = os.path.join(os.path.dirname(__file__), 'pipe.pkl')
-file_path2 = os.path.join(os.path.dirname(__file__), 'df.pkl')
-
-clf = pickle.load(open(file_path, 'rb'))
-df = pickle.load(open(file_path2, 'rb'))
-
 # import the model
-# # clf = pickle.load(open('pipe.pkl','rb'))
-# df = pickle.load(open('df.pkl','rb'))
-# # df = pickle.load(open('pipe.pkl','rb'))
-
+clf = pickle.load(open('pipe.pkl','rb'))
+df = pickle.load(open('df.pkl','rb'))
 
 # Initialize a flag to check if any condition is violated
 conditions_violated = False
@@ -24,53 +14,86 @@ st.markdown("# <div style='text-align: center;color:aqua'>CROP ADVISOR</div>", u
 
 st.markdown("")
 st.markdown("")
-N = st.text_input("##### Enter ratio of Nitrogen content in soil : ")
+N = st.text_input("##### Enter ratio of Nitrogen content in soil :  [ Range from 1 to 140 ]")
 # Check if N is not an empty string and is a valid float
-if N and not (0 < int(N) < 140):
-    st.markdown("<p style='text-align: center;color:red;font-size: 20px'>No Crop is suitable to grow in this type of Nitrogen content.</p>", unsafe_allow_html=True)
+# Check if N is not an empty string and is a valid integer
+try:
+    if N and not (0 < int(N) <= 140):
+        st.markdown("<p style='text-align: center;color:red;font-size: 20px'>No Crop is suitable to grow in this type of Nitrogen content.</p>", unsafe_allow_html=True)
+        conditions_violated = True
+except ValueError:
+    st.markdown("<p style='text-align: center;color:red;font-size: 20px'>Please enter a valid integer for Nitrogen content.</p>", unsafe_allow_html=True)
     conditions_violated = True
 
 st.markdown("")
-P = st.text_input("##### Enter ratio of Phosphorous content in soil : ")
+P = st.text_input("##### Enter ratio of Phosphorous content in soil : [ Range from 5 to 145 ]")
 # Check if P is not an empty string and is a valid float
-if P and not (5 < int(P) < 145):
-    st.markdown("<p style='text-align: center;color:red;font-size: 20px'>No Crop is suitable to grow in this type of Phosphorous content.</p>", unsafe_allow_html=True)
+# Similar try-except blocks for P and K
+try:
+    if P and not (5 <= int(P) <= 145):
+        st.markdown("<p style='text-align: center;color:red;font-size: 20px'>No Crop is suitable to grow in this type of Phosphorous content.</p>", unsafe_allow_html=True)
+        conditions_violated = True
+except ValueError:
+    st.markdown("<p style='text-align: center;color:red;font-size: 20px'>Please enter a valid integer for Phosphorous content.</p>", unsafe_allow_html=True)
     conditions_violated = True
 
 st.markdown("")
-K = st.text_input("##### Enter ratio of Potassium content in soil : ")
+K = st.text_input("##### Enter ratio of Potassium content in soil :  [ Range from 5 to 205 ] ")
 
 # Check if K is not an empty string and is a valid float
-if K and not (5 < int(K) < 205):
-    st.markdown("<p style='text-align: center;color:red;font-size: 20px'>No Crop is suitable to grow in this type of Potassium content.</p>", unsafe_allow_html=True)
+try:
+    if K and not (5 <= int(K) <= 205):
+        st.markdown("<p style='text-align: center;color:red;font-size: 20px'>No Crop is suitable to grow in this type of Potassium content.</p>", unsafe_allow_html=True)
+        conditions_violated = True
+except ValueError:
+    st.markdown("<p style='text-align: center;color:red;font-size: 20px'>Please enter a valid integer for Potassium content.</p>", unsafe_allow_html=True)
     conditions_violated = True
 
 st.markdown("")
-Temp = st.text_input("##### Enter Temperature (in °C) :")
+Temp = st.text_input("##### Enter Temperature (in °C) :  [ Range from 0 to 50 ]")
 # Check if Temp is not an empty string and is a valid float
-if Temp and not (0 < float(Temp) < 50):
-    st.markdown("<p style='text-align: center;color:red;font-size: 20px'>No Crop is suitable to grow in this type of Temperature condition.</p>", unsafe_allow_html=True)
+# Check if Temp is not an empty string and is a valid float
+try:
+    if Temp and not (0 <= float(Temp) <= 50):
+        st.markdown("<p style='text-align: center;color:red;font-size: 20px'>No Crop is suitable to grow in this type of Temperature condition.</p>", unsafe_allow_html=True)
+        conditions_violated = True
+except ValueError:
+    st.markdown("<p style='text-align: center;color:red;font-size: 20px'>Please enter a valid interger of floating number for Temperature.</p>", unsafe_allow_html=True)
     conditions_violated = True
 
 st.markdown("")
-Humid = st.text_input("##### Enter value of relative Humidity (in %) :")
+Humid = st.text_input("##### Enter value of relative Humidity (in %) :  [ Range from 0 to 100 ]")
 # Check if Humid is not an empty string and is a valid float
-if Humid and not (0 < float(Humid) < 100):
-    st.markdown("<p style='text-align: center;color:red;font-size: 20px'>Enter the percentage of Humidity between 1 to 100.</p>", unsafe_allow_html=True)
+# Check if Humid is not an empty string and is a valid float
+try:
+    if Humid and not (0 <= float(Humid) <= 100):
+        st.markdown("<p style='text-align: center;color:red;font-size: 20px'>Enter the percentage of Humidity between 1 to 100.</p>", unsafe_allow_html=True)
+        conditions_violated = True
+except ValueError:
+    st.markdown("<p style='text-align: center;color:red;font-size: 20px'>Please enter a valid valid interger of floating number for Humidity.</p>", unsafe_allow_html=True)
     conditions_violated = True
 
 st.markdown("")
-ph = st.text_input("##### Enter pH value in soil : ")
+ph = st.text_input("##### Enter pH value in soil :  [ Range from 0 to 14 ] ")
 # Check if ph is not an empty string and is a valid float
-if ph and not (0 < float(ph) < 14):
-    st.markdown("<p style='text-align: center;color:red;font-size: 20px'>No Crop is suitable to grow in this type of pH condition.</p>", unsafe_allow_html=True)
+try:
+    if ph and not (0 <= float(ph) <= 14):
+        st.markdown("<p style='text-align: center;color:red;font-size: 20px'>No Crop is suitable to grow in this type of pH condition.</p>", unsafe_allow_html=True)
+        conditions_violated = True
+except ValueError:
+    st.markdown("<p style='text-align: center;color:red;font-size: 20px'>Please enter a valid interger of floating number for pH.</p>", unsafe_allow_html=True)
     conditions_violated = True
 
+
 st.markdown("")
-rain = st.text_input("##### Enter value of Rainfall (in mm) :")
+rain = st.text_input("##### Enter value of Rainfall (in mm) :  [ Range from 15 to 300 ]")
 # Check if rain is not an empty string and is a valid float
-if rain and not (15 < float(rain) < 300):
-    st.markdown("<p style='text-align: center;color:red;font-size: 20px'>No Crop is suitable to grow in this type of Rainfall condition.</p>", unsafe_allow_html=True)
+try:
+    if rain and not (15 <= float(rain) <= 300):
+        st.markdown("<p style='text-align: center;color:red;font-size: 20px'>No Crop is suitable to grow in this type of Rainfall condition.</p>", unsafe_allow_html=True)
+        conditions_violated = True
+except ValueError:
+    st.markdown("<p style='text-align: center;color:red;font-size: 20px'>Please enter a valid integer or float value for Rainfall.</p>", unsafe_allow_html=True)
     conditions_violated = True
 
 # If any condition is violated, don't proceed with the prediction
@@ -82,4 +105,3 @@ else:
         query = np.array([float(N), float(P), float(K), float(Temp), float(Humid), float(ph), float(rain)])
         query = query.reshape(1, 7)
         st.markdown("<h1 style='color:#a5f10b;'>The Suitable Crop to grow in these conditions is " + clf.predict(query)[0].capitalize() + ".</h1>", unsafe_allow_html=True)
-
