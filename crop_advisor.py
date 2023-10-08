d05fd3220eaa098a92698e236574bee8e9709fb8
KalashShah90 <138599698+KalashShah90@users.noreply.github.com>
2023-10-08 09:56:22 +0530

    Update crop_advisor.py

--- crop_advisor.py
+++ crop_advisor.py
@@ -2,6 +2,12 @@ import streamlit as st
 import pickle
 import numpy as np
 
+# Define theme colors and font
+backgroundColor = "#0E1117"
+textColor = "#FAFAFA"
+font = "sans-serif"
+secondaryBackgroundColor = "#262730"
+
 # Add custom CSS
 custom_styles = f"""
     <style>
@@ -16,14 +22,8 @@ custom_styles = f"""
     </style>
 """
 
-# Define theme colors and font
-backgroundColor = "#0E1117"
-textColor = "#FAFAFA"
-font = "sans-serif"
-secondaryBackgroundColor = "#262730"
-
 # Apply custom styles
-st.markdown(custom_styles, unsafe_allow_html=True)
+st.markdown(custom_styles, unsafe_allow_html=True
 
 # import the model
 clf = pickle.load(open('pipe.pkl','rb'))
