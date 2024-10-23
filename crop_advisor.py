e627a708c854c1c476489c55bbe7259798a6f76f
Kalash Ketanbhai Shah <138599698+Kalash9630@users.noreply.github.com>
2024-10-24 00:54:26 +0530

    Update crop_advisor.py

--- crop_advisor.py
+++ crop_advisor.py
@@ -10,7 +10,8 @@ df = pickle.load(open('df.pkl','rb'))
 conditions_violated = False
 
 # title
-st.markdown("<div style='text-align: center;color:#ADD8E6;font-weight:bold;font-size:60px'>CROP ADVISOR</div>", unsafe_allow_html=True)
+st.markdown("<div style='text-align: center;color:#0000FF;font-weight:bold;font-size:60px'>CROP ADVISOR</div>", unsafe_allow_html=True)
+
 
 st.markdown("")
 st.markdown("")
