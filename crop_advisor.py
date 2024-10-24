0190780fcf816280c4b7dbef5a45acd8d911b465
Kalash Ketanbhai Shah <138599698+Kalash9630@users.noreply.github.com>
2024-10-24 14:50:43 +0530

    Update crop_advisor.py

--- crop_advisor.py
+++ crop_advisor.py
@@ -12,37 +12,37 @@ st.markdown("<h1 style='text-align: center;'>CROP ADVISOR</h1>", unsafe_allow_ht
 st.markdown("")
 
 # Input fields with validations
-N = st.text_input("##### Enter ratio of Nitrogen content in soil: [Range from 1 to 140]", key="nitrogen_input")
+N = st.text_input("##### Enter ratio of Nitrogen content in soil:  [Range from 1 to 140]", key="nitrogen_input")
 if N and (float(N) < 1 or float(N) > 140):
     st.markdown("<h6 style='color: red;'>Nitrogen content must be between 1 and 140.</h6>", unsafe_allow_html=True)
 
 st.markdown("")
-P = st.text_input("##### Enter ratio of Phosphorous content in soil: [Range from 5 to 145]", key="phosphorous_input")
+P = st.text_input("##### Enter ratio of Phosphorous content in soil:  [Range from 5 to 145]", key="phosphorous_input")
 if P and (float(P) < 5 or float(P) > 145):
     st.markdown("<h6 style='color: red;'>Phosphorous content must be between 5 and 145.</h6>", unsafe_allow_html=True)
 
 st.markdown("")
-K = st.text_input("##### Enter ratio of Potassium content in soil: [Range from 5 to 205]", key="potassium_input")
+K = st.text_input("##### Enter ratio of Potassium content in soil:  [Range from 5 to 205]", key="potassium_input")
 if K and (float(K) < 5 or float(K) > 205):
     st.markdown("<h6 style='color: red;'>Potassium content must be between 5 and 205.</h6>", unsafe_allow_html=True)
 
 st.markdown("")
-Temp = st.text_input("##### Enter Temperature (in °C): [Range from 0 to 50]", key="temperature_input")
+Temp = st.text_input("##### Enter Temperature (in °C):  [Range from 0 to 50]", key="temperature_input")
 if Temp and (float(Temp) < 0 or float(Temp) > 50):
     st.markdown("<h6 style='color: red;'>Temperature must be between 0 and 50 °C.</h6>", unsafe_allow_html=True)
 
 st.markdown("")
-Humid = st.text_input("##### Enter value of relative Humidity (in %): [Range from 0 to 100]", key="humidity_input")
+Humid = st.text_input("##### Enter value of relative Humidity (in %):  [Range from 0 to 100]", key="humidity_input")
 if Humid and (float(Humid) < 0 or float(Humid) > 100):
     st.markdown("<h6 style='color: red;'>Humidity must be between 0 and 100%.</h6>", unsafe_allow_html=True)
 
 st.markdown("")
-ph = st.text_input("##### Enter pH value in soil: [Range from 0 to 14]", key="ph_input")
+ph = st.text_input("##### Enter pH value in soil:  [Range from 0 to 14]", key="ph_input")
 if ph and (float(ph) < 0 or float(ph) > 14):
     st.markdown("<h6 style='color: red;'>pH value must be between 0 and 14.</h6>", unsafe_allow_html=True)
 
 st.markdown("")
-rain = st.text_input("##### Enter value of Rainfall (in mm): [Range from 15 to 300]", key="rainfall_input")
+rain = st.text_input("##### Enter value of Rainfall (in mm):  [Range from 15 to 300]", key="rainfall_input")
 if rain and (float(rain) < 15 or float(rain) > 300):
     st.markdown("<h6 style='color: red;'>Rainfall must be between 15 and 300 mm.</h6>", unsafe_allow_html=True)
 
