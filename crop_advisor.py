1b9895f21e3feb3c633e2a9971c33bf30fcf1912
Kalash Ketanbhai Shah <138599698+Kalash9630@users.noreply.github.com>
2024-11-08 17:28:11 +0530

    Update crop_advisor.py

--- crop_advisor.py
+++ crop_advisor.py
@@ -9,7 +9,7 @@ df = pickle.load(open('df.pkl', 'rb'))
 # Title
 st.markdown("<h1 style='font-size: 50px; text-align: center;'>CROP ADVISOR</h1>", unsafe_allow_html=True)
 st.markdown("")
-
+st.markdown("")
 
 st.markdown('<p style="font-size: 21px ; margin-bottom: -50px">Enter ratio of Nitrogen content in soil:  [Range from 1 to 140]</p>', unsafe_allow_html=True)
 N = st.text_input("", key="nitrogen_input")
@@ -17,37 +17,37 @@ if N and (float(N) < 1 or float(N) > 140):
     st.markdown("<h6 style='color: red;'>Nitrogen content must be between 1 and 140.</h6>", unsafe_allow_html=True)
 
 st.markdown("")
-st.markdown('<p style="font-size: 22px ; margin-bottom: -50px">Enter ratio of Phosphorous content in soil:  [Range from 5 to 145]</p>', unsafe_allow_html=True)
+st.markdown('<p style="font-size: 21px ; margin-bottom: -50px">Enter ratio of Phosphorous content in soil:  [Range from 5 to 145]</p>', unsafe_allow_html=True)
 P = st.text_input("", key="phosphorous_input")
 if P and (float(P) < 5 or float(P) > 145):
     st.markdown("<h6 style='color: red;'>Phosphorous content must be between 5 and 145.</h6>", unsafe_allow_html=True)
 
 st.markdown("")
-st.markdown('<p style="font-size: 22px ; margin-bottom: -50px">Enter ratio of Potassium content in soil:  [Range from 5 to 205]</p>', unsafe_allow_html=True)
+st.markdown('<p style="font-size: 21px ; margin-bottom: -50px">Enter ratio of Potassium content in soil:  [Range from 5 to 205]</p>', unsafe_allow_html=True)
 K = st.text_input("", key="potassium_input")
 if K and (float(K) < 5 or float(K) > 205):
     st.markdown("<h6 style='color: red;'>Potassium content must be between 5 and 205.</h6>", unsafe_allow_html=True)
 
 st.markdown("")
-st.markdown('<p style="font-size: 22px ; margin-bottom: -50px"> Enter Temperature (in °C):  [Range from 0 to 50]</p>', unsafe_allow_html=True)
+st.markdown('<p style="font-size: 21px ; margin-bottom: -50px"> Enter Temperature (in °C):  [Range from 0 to 50]</p>', unsafe_allow_html=True)
 Temp = st.text_input("", key="temperature_input")
 if Temp and (float(Temp) < 0 or float(Temp) > 50):
     st.markdown("<h6 style='color: red;'>Temperature must be between 0 and 50 °C.</h6>", unsafe_allow_html=True)
 
 st.markdown("")
-st.markdown('<p style="font-size: 22px ; margin-bottom: -50px"> Enter value of relative Humidity (in %):  [Range from 0 to 100]', unsafe_allow_html=True)
+st.markdown('<p style="font-size: 21px ; margin-bottom: -50px"> Enter value of relative Humidity (in %):  [Range from 0 to 100]', unsafe_allow_html=True)
 Humid = st.text_input("", key="humidity_input")
 if Humid and (float(Humid) < 0 or float(Humid) > 100):
     st.markdown("<h6 style='color: red;'>Humidity must be between 0 and 100%.</h6>", unsafe_allow_html=True)
 
 st.markdown("")
-st.markdown('<p style="font-size: 22px ; margin-bottom: -50px"> Enter pH value in soil:  [Range from 0 to 14]', unsafe_allow_html=True)
+st.markdown('<p style="font-size: 21px ; margin-bottom: -50px"> Enter pH value in soil:  [Range from 0 to 14]', unsafe_allow_html=True)
 ph = st.text_input("", key="ph_input")
 if ph and (float(ph) < 0 or float(ph) > 14):
     st.markdown("<h6 style='color: red;'>pH value must be between 0 and 14.</h6>", unsafe_allow_html=True)
 
 st.markdown("")
-st.markdown('<p style="font-size: 22px ; margin-bottom: -50px"> Enter value of Rainfall (in mm):  [Range from 15 to 300]', unsafe_allow_html=True)
+st.markdown('<p style="font-size: 21px ; margin-bottom: -50px"> Enter value of Rainfall (in mm):  [Range from 15 to 300]', unsafe_allow_html=True)
 rain = st.text_input("", key="rainfall_input")
 if rain and (float(rain) < 15 or float(rain) > 300):
     st.markdown("<h6 style='color: red;'>Rainfall must be between 15 and 300 mm.</h6>", unsafe_allow_html=True)
